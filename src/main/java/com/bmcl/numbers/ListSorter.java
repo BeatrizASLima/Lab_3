74cc287edb8dcaa56de3b352512d19c791a3cb26
beatriz <a044416@umaia.pt>
2024-11-28 16:03:08 +0000

    deduplocate

--- src/main/java/com/bmcl/numbers/ListSorter.java
+++ src/main/java/com/bmcl/numbers/ListSorter.java
@@ -1,42 +1,47 @@
 package com.bmcl.numbers;
 
 import java.util.ArrayList;
-
 import java.util.Collections;
 import java.util.List;
 
 /**
- * utility class to sort list of numbers.
+ * Utility class to sort and deduplicate a list of numbers.
  */
-public class ListSorter {
-        public List<Integer> deduplicate(List<Integer> list) {
-            List<Integer> sorted = sorter.sort(list);
-            List<Integer> unique = new ArrayList<>();
-            Integer last = null;
-            for (Integer number : sorted)
-                if (!number.equals(last)) {
-                    last = number;
-                    unique.add(number);
-                }
-            return unique;
-        }
+public class ListSorter implements GenericListSorter {
+
     /**
-     * Really stupid way to sort a list.
+     * Removes duplicates from the given list after sorting it.
      *
-     * @return A sorted version of the list.
+     * @param list the list of integers to deduplicate.
+     * @return a sorted list with unique integers.
+     */
+    public List<Integer> deduplicate(List<Integer> list) {
+        List<Integer> sorted = sort(list);
+        List<Integer> unique = new ArrayList<>();
+        Integer last = null;
+        for (Integer number : sorted) {
+            if (!number.equals(last)) {
+                last = number;
+                unique.add(number);
+            }
+        }
+        return unique;
+    }
+
+    /**
+     * Sorts the given list of integers in ascending order.
+     *
+     * @param list the list to be sorted.
+     * @return a sorted version of the list.
      */
     public List<Integer> sort(List<Integer> list) {
         List<Integer> sorted = new ArrayList<>();
         for (Integer number : list)
             sorted.add(number);
-
         for (int i = 0; i < sorted.size() - 1; i++)
             for (int j = i + 1; j < sorted.size() - 1; j++)
                 if (sorted.get(i) > sorted.get(j))
                     Collections.swap(sorted, i, j);
-
         return sorted;
     }
 }
-    //COMECAÇA O EXERCICIO
-
