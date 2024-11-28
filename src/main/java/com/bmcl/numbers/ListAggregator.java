5aefea4c093bf799f23b1c68c289db7fb59b6306
beatriz <a044416@umaia.pt>
2024-11-28 15:40:36 +0000

    AggregatorTes

--- src/main/java/com/bmcl/numbers/ListAggregator.java
+++ src/main/java/com/bmcl/numbers/ListAggregator.java
@@ -54,7 +54,7 @@ public class ListAggregator {
      *
      * @return The number of distinct numbers.
      */
-    public int distinct(List<Integer> list, GenericListDeduplicator deduplicator) {
+    public int distinct(List<Integer> list) {
         List<Integer> distinct = deduplicator.deduplicate(list);
         return distinct.size();
     }
